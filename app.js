309fb48c62c31a2720f5fe798220167e3d21e716
Richard Batista <stalfos@icloud.com>
2018-04-26 17:59:00 -0400

    Completed iteration 2

--- app.js
+++ app.js
@@ -1,31 +1,48 @@
-// app.js
 const express = require('express')
 const app     = express()
 const hbs     = require('hbs')
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/video').then(() => {
+	console.log('Connected to Mongo!')
+  }).catch(err => {
+	console.error('Error connecting to mongo', err)
+});
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.get('/', function (req, res) {
-  console.log(req)
-})
+const Schema   = mongoose.Schema;
+const movieSchema = new Schema({
+	title : String,
+  	year  : String,
+  	director: String,
+  	duration: String,
+  	genre: [],
+  	rate : String
+});
 
-app.get('/users/:username', function (req, res) {
+const Movie = mongoose.model('Movie', movieSchema);
+
+app.get('/', function (req, res) {
+	Movie.find({}, function(err, movie) {
+		res.render('index', {movie});
+	});
+});
+
+app.get('/movies/:director', function (req, res) {
   let theUserName = req.params.username;
   // let theUser = User.find({username: theUserName})   this is how we will query the database
   let data = {theActualUserName: theUserName }
   res.render('userpage', data)
 })
 
-
-app.get('/search', function (req, res) {
-  res.send(req.query)
+app.get('/movies/:year', function (req, res) {
+  let theUserName = req.params.username;
+  // let theUser = User.find({username: theUserName})   this is how we will query the database
+  let data = {theActualUserName: theUserName }
+  res.render('userpage', data)
 })
-
-
-
-
-
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
