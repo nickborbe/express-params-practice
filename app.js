aa0140308000a3e1fb93ef59076622edf9b005a5
Claudia Hernandez <hernandez.clauds@gmail.com>
2018-04-27 18:17:10 -0400

    new updated friday

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const app     = express()
 const hbs     = require('hbs')
 const path = require('path');
 const mongoose = require('mongoose');
-
 mongoose.Promise = global.Promise;
+const Schema   = mongoose.Schema;
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 mongoose.connect('mongodb://localhost/video')
   .then(() => {
     console.log('Connected to Mongo!')
@@ -13,45 +17,130 @@ mongoose.connect('mongodb://localhost/video')
     console.error('Error connecting to mongo', err)
   });
 
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-const Schema   = mongoose.Schema;
 
 const movieSchema = new mongoose.Schema({
   title : String,
   year  : String,
   director: String,
   duration: String,
-  genre: [],
+  genre: [String],
   rate : String
 });
 
 const Movie = mongoose.model('Movie', movieSchema);
 
-app.get('/index', function (req, res) {
-  Movie.find({}, function(err, movie) {
-    res.render('index', {movie});
+app.get('/movies', function (req, res) {
+  Movie.find()
+  .then(movies => {
+    let data = {};
+    data.theList = movies;
+    res.render('index', data)
+  })
+  .catch(theError => { console.log(theError) })
+})
+
+
+app.get('/movies/new', function (req, res) {
+  res.render('createMovie')
+})
+
+app.post('/movies/create', function (req, res) {
+console.log("req body", req.body);
+
+const theActualTitle = req.body.theTitle;
+const theActualDirector = req.body.theDirector;
+const theActualYear = req.body.theYear;
+const theActualRate = req.body.theRate;
+
+const newMovie = new Movie({
+title : theActualTitle,
+director: theActualDirector,
+year  : theActualYear,
+rate: theActualRate
+})
+console.log("about to save new movie!!")
+newMovie.save()
+.then(movie => {
+console.log("Saved ne moveie!")
+console.log(movie)
+})
+.catch(theError => { 
+  console.log(theError) 
+  assert.isNotOk(error,'Promise error');
+})
+
+res.redirect('/movies')
+});
+
+app.get('/movies/edit/:id', function (req, res) {
+  Movie.findById(req.params.id)
+  .then(theMovie =>{
+    res.render('editMovie', {movie: theMovie})
+  })
+});
+
+app.post('/movies/update/:id', function (req, res) {
+Movie.findByIdAndUpdate(req.params.id, {
+  title: req.body.title,
+  director: req.body.director,
+  year: req.body.year,
+  rate: req.body.rate
+})
+.then(movie => {
+  console.log(movie);
+})
+.catch(theError => { console.log(theError) })
+
+res.redirect('/movies')
+});
+
+
+app.post('/movies/delete/:id', function(req, res){
+  const movieId = req.params.id;
+  Movie.findByIdAndRemove(movieId)
+  .then(movie => {
+    console.log(movie);
+  })
+  .catch(error => {
+    console.log(error);
+  })
+res.redirect('/movies')
+});
+
+app.get('/movies/director/:director', function (req, res) {
+    const theDirector = req.params.director
+    Movie.find({director: theDirector}, function(err, movie) {
+    res.render('moviesbydirector', {movie});
  });
 
 });
 
-app.post("/index", (req, res) => {
- 
+app.get('/movies/:theIdthing', function (req, res) {
+  const theId = req.params.theIdthing
+  Movie.findById(theId)
+  .then(theMovie => {
+    console.log(theMovie);
+    res.render('movieshow', {theMovie});
+  })
 });
 
 
-app.get('/users/:username', function (req, res) {
-  let theUserName = req.params.username;
-  let theUser = User.find({username: theUserName})  //  this is how we will query the database
-  let data = {theActualUserName: theUserName }
-  res.render('userpage', data)
-})
+
+app.get('/movies/year/:year', function (req, res) {
+  const theYear = req.params.year
+  Movie.find({year: theYear}, function(err, movie) {
+  res.render('moviebyyear', {movie});
+});
+});
 
 
-app.get('/search', function (req, res) {
-  res.send(req.query)
-})
+
+
+
 
 
 
@@ -69,5 +158,3 @@ app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
 // 4. make a route that looks like this ('/movies/:year')
 // that displays a list of all movies that were made in the year that is typed into the url
-
-
