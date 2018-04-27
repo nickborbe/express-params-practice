be4320ad22ca2f39fc56b8cc32212445248981a9
Eleyni Rodriguez <eleyni.m.rodriguez@gmail.com>
2018-04-27 09:05:10 -0400

    WIP. Index and director pages are working. Bug fixing the year page.

--- app.js
+++ app.js
@@ -1,30 +1,60 @@
+const mongoose = require('mongoose');
+// const movieData = require('./movies.json');
+
 // app.js
 const express = require('express')
 const app     = express()
 const hbs     = require('hbs')
 const path = require('path');
 
+mongoose.connect('mongodb://localhost/video')
+  .then(() => {
+    console.log('Connected to Mongo!')
+  }).catch(err => {
+    console.error('Error connecting to mongo', err)
+  });
+
+const Schema = mongoose.Schema;
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.get('/', function (req, res) {
-  console.log(req)
+
+const movieSchema = new Schema({
+  title    : String,
+  year     : Number,
+  director : String,
+  duration : String,
+  genre    : [String],
+  rate     : Number
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+
+// Routes
+app.get('/', (req, res) => {
+  Movie.find({}, function(err, movie) {
+    console.log(' Movie: ', movie.title, movie.director, movie.year, movie.rating);
+    res.render('index', {movie})
+  });
+});
+
+app.get('/movies/:director', function (req, res) {
+  let theDirector = req.params.director;
+  let data = {theDirectorsName: theDirector }
+  Movie.find({director: theDirector}, function(err, movie) {
+    res.render('moviepage', {movie})
+  });
 })
 
-app.get('/users/:username', function (req, res) {
-  let theUserName = req.params.username;
-  // let theUser = User.find({username: theUserName})   this is how we will query the database
-  let data = {theActualUserName: theUserName }
-  res.render('userpage', data)
+app.get('/movies/:year', function (req, res) {
+  let theYear = req.params.year;
+  let data = {theMovieYear: theYear }
+  Movie.find({year: theYear}, function(err, movie) {
+    res.render('moviepage', {movie})
+  });
 })
-
-
-app.get('/search', function (req, res) {
-  res.send(req.query)
-})
-
-
-
 
 
 
