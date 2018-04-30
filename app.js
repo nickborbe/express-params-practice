4cc0dc5451ef580778351f3130b096b6f8fbb3f1
Crystal Meneses <crystalm927@gmail.com>
2018-04-29 23:58:52 -0400

    changed a few things

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-
-
 // app.js
 const express = require('express')
 const app     = express()
 const hbs     = require('hbs')
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 mongoose.connect('mongodb://localhost/video');
 const Schema   = mongoose.Schema;
 
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const movieSchema = new Schema({
   title :  String ,
@@ -78,48 +79,69 @@ app.get('/movies/year/:year', function (req, res) {
 })
 
 
+app.get('/movies/new', (req, res) => {
+  res.render('newMovie');
+})
+
 
 app.post('/movies/create', function (req, res) {
   console.log("req body", req.body);
 
-const theActualTitle = req.body.thetitle;
-const theActualDirector = req.body.theDirector;
-const theActualYear = req.body.theYear;
+  const newMovie = new Movie ({
+  title: req.body.theNewTitle,
+  year: req.body.theNewYear,
+  director: req.body.theNewDirector,
+  })
 
+  newMovie.save()
+  .then( movie => {console.log(movie)})
+  .catch( err => {res.send(err)})
 
-const newMovie = new Movie({
-  title : theActualTitle ,
-  director: theActualDirector,
-  year  : theActualYear,
+  res.redirect('/movies');
 })
 
-newMovie.save()
-.then(movie => {
-  // console.log(car);
-})
-.catch(theError => { console.log(theError) })
-
-res.redirect('/movies')
-});
 
 
 
-app.post('/movies/delete/:id', function(req, res){
+app.post('/movies/delete/:id', (req, res) => {
   const movieId = req.params.id;
   Movie.findByIdAndRemove(movieId)
+  .then(movie => {
+    console.log(movie);
+  })
+  .catch(error => {
+    console.log(error);
+  })
+res.redirect('/movies')
+})
 
-    .then(movie => {
-      console.log(movie);
-    })
-
-    .catch(error => {
-      console.log(error);
-    });
-    res.redirect('/movies')
+app.get('/movies/edit/:id', (req, res) => {
+  Movie.findById(req.params.id)
+  .then(theMovie =>{
+    res.render('editMovie', {movie: theMovie})
+  })
 })
 
 
 
+app.post('/movies/update/:id', (req, res) => {
+  Movie.findByIdAndUpdate(req.params.id, {
+    title: req.body.title,
+    year: req.body.year,
+    director: req.body.director,
+    duration: req.body.duration,
+    genre: req.body.genre,
+    rate: req.body.rate
+  })
+  .then(movie => {
+    console.log(movie);
+    res.redirect(`/movies/select/${movie._id}`)
+  })
+  .catch(theError => { console.log(theError) })
+  
+  
+  })
+  
 
 
 
